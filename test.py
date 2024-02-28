25a2482910fdb8c624b704ff52c05557f5bcf201
konstantinovaaa <konstantinovaa21@st.ithub.ru>
2024-02-28 11:47:20 +0300

    first commit

---
+++ test.py
@@ -0,0 +1,37 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import Select
+
+def main():
+    driver = webdriver.Chrome()
+    driver.get("http://localhost")
+    wait = WebDriverWait(driver, 100)
+
+    product_element = wait.until(EC.presence_of_element_located((By.XPATH, "//div[@class='product']")))
+    product_element.click()
+
+    screenshot_element = wait.until(EC.presence_of_element_located((By.XPATH, "//div[@class='screenshot']")))
+    screenshot_element.click()
+
+    currency_select = Select(driver.find_element(By.ID, "currency-select"))
+    currency_select.select_by_value("EUR")
+
+    pc_menu_item = wait.until(EC.element_to_be_clickable((By.XPATH, "//li[@class='menu-item']/a[contains(text(), 'PC')]")))
+    pc_menu_item.click()
+
+    registration_link = wait.until(EC.element_to_be_clickable((By.XPATH, "//a[@href='/registration']")))
+    registration_link.click()
+
+    search_keyword = "search_keyword"
+    search_input = driver.find_element(By.ID, "search-input")
+    search_input.send_keys(search_keyword)
+
+    search_button = driver.find_element(By.ID, "search-button")
+    search_button.click()
+
+    driver.quit()
+
+if __name__ == "__main__":
+    main()
